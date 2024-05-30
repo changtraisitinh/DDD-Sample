693cbbb87de28973a2528c0ccdccc5b347d96bb9
Hiep Nguyen <nguyenhuuhiep1990@gmail.com>
2024-05-30 16:37:13 +0700

    Integrate OpenAPI VietQR

--- src/main/java/com/example/DDDSample/application/configuration/ApplicationConfig.java
+++ src/main/java/com/example/DDDSample/application/configuration/ApplicationConfig.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableConfigurationProperties
@@ -16,5 +18,10 @@ public class ApplicationConfig {
     @Getter
     public static class Signature {
 
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
